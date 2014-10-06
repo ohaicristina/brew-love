b7dd657b53ce9d907c1967af71e852c75e6549a9
ohaicristina <casilva62592@gmail.com>
2014-10-06 19:45:41 -0400

    Add config.ru

--- config.rb
+++ config.rb
@@ -73,23 +73,3 @@ configure :build do
   # set :http_prefix, "/Content/images/"
 end
 
-require 'rack/contrib/try_static'
-
-use Rack::Deflater
-use Rack::TryStatic,
-    root: 'tmp',
-      urls: %w[/],
-        try: %w[.html index.html /index.html]
-
-FIVE_MINUTES=300
-
-run lambda { |env|
-    [
-          404,
-              {
-                  'Content-Type'  => 'text/html',
-                        'Cache-Control' => "public, max-age=#{FIVE_MINUTES}"
-              },
-                  ['File not found']
-                    ]
-}
