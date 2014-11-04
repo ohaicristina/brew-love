cdde4aa42aadfceffb522189d9809e0ec849f754
ohaicristina <casilva62592@gmail.com>
2014-11-04 17:57:26 -0500

    Add basic off-canvas functionality after much struggles

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,6 +4,21 @@
 
 $(document).ready(function () {
   console.log("Testing123");
-});
 
-$('#main').smoothState();
+  $(function() {
+    $('.menu-button').click(function() {
+      // Calling a function in case you want to expand upon this.
+      toggleNav();
+    });
+  });
+
+  function toggleNav() {
+    if ($('.offcanvas-menu').hasClass('show-nav')) {
+      $('.offcanvas-menu').removeClass('show-nav');
+    } else {
+      $('.offcanvas-menu').addClass('show-nav');
+    }
+
+    //$('#site-wrapper').toggleClass('show-nav');
+  }
+});
