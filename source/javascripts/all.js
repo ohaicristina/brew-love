8799d3197ba70245cb8d1e00198e85494b4aade6
ohaicristina <casilva62592@gmail.com>
2014-11-19 01:15:20 -0500

    GOT JSON TO WORK OHYES

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,6 +4,7 @@
 
 $(document).ready(function () {
   console.log("Testing123");
+  listBeers();
 
   $(function() {
     $('.show-offcanvas').click(function() {
@@ -21,45 +22,36 @@ $(document).ready(function () {
     }
   }
 
-  function updateApi() {
-    apiString = "../data/beerStyles.json";
+  function listBeers() {
+    apiString = "data/beerStyles.json";
 
     $.getJSON(apiString, function(data) {
-
       beers = data.beers;
-      $(".artists li").remove();
-
-      $.each(artists, function(i) {
-        $(".artists").append("<li data-score='" + data.response.artists[i].hotttnesss + "'>" + data.response.artists[i].name + "</li>");
-        //$(".artists").append("<li>" + data.response.artists[i].hotttnesss + "</li>");
+      $(".characteristic p").remove();
+      $.each(beers, function(i) {
+        $(".lagers").append('<li class="menu-item-box">' + data.beers[i].name + '</li>');
       });
-    })
+      $.each(beers, function(i) {
+        $("#history").append("<p>" + data.beers[0].history + "</p>");
+        $("#color").append("<p>" + data.beers[0].color + "</p>");
+        $("#flavor").append("<p>" + data.beers[0].flavor + "</p>");
+        $("#hops").append("<p>" + data.beers[0].hops + "</p>");
+        $("#malt").append("<p>" + data.beers[0].malt + "</p>");
+      });
+        // $(".recommendations").append("<li>" + data.beers[0].reccomendation + "</li>");
 
-    .done(function(data) {
-      $("li").each(function(i) {
+      });
+    
 
-        if ($(this).attr("data-score") >= 0.8) {
-          $(this).addClass("redcircle");
-        }
-
-        else {
-          $(this).addClass("bluetext");
-        }
-
-
-      })
-    });
-
+    // .done(function(data) {
+    //   $("li").each(function(i) {
+    //     if ($(this).attr("data-score") >= 0.8) {
+    //       $(this).addClass("redcircle");
+    //     }
+    //     else {
+    //       $(this).addClass("bluetext");
+    //     }
+    //   })
+    // });
   }
-
-  $("input").on({
-
-    change: function() {
-      inputGenre = $(this).val();
-      updateApi();
-    }
-
-  });
-
-
 });
