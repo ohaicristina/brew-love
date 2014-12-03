b76d1fcf3c8240e58f41cd45522aa53931c2a3ee
ohaicristina <casilva62592@gmail.com>
2014-12-03 01:02:51 -0500

    handlebars and things

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,10 +1,27 @@
 //= require jquery
-//= require_tree .
 //= require jquery.smoothState.js
+//= require handlebars-v2.0.0.js
+//= require_tree .
 
 $(document).ready(function () {
   console.log("Testing123");
   listBeers();
+
+
+
+  //Active class toggle
+  $(function() {
+    $('.water-col').click(function() {
+      toggleWaterActive();
+    });
+  });
+
+  function toggleWaterActive() {
+    var $waterh3 = $('.water-col h3').click(function(){
+      $waterh3.removeClass('water-active');
+      $(this).addClass('water-active');
+    });
+  }
 
   //Off-canvas toggle
   $(function() {
@@ -30,9 +47,11 @@ $(document).ready(function () {
     $.getJSON(apiString, function(data) {
       beers = data.beers;
       $(".characteristic p").remove();
+
       $.each(beers, function(i) {
         $(".lagers").append('<a class="beer-link" href="#"  id="link"><li class="menu-item-box">' + data.beers[i].name + '</li></a>');
       });
+
       $.each(beers, function(b) {
         $("#history").append("<p>" + data.beers[b].history + "</p>");
         $("#color").append("<p>" + data.beers[b].color + "</p>");
@@ -61,4 +80,14 @@ $(document).ready(function () {
     //   })
     // });
   }
+});
+
+//Handlbars attempt eep.
+$(function  () {
+  var waterData = "data/waterProperties.json";
+   //Get the HTML from the template   in the script tag
+    var waterTemplateScript = $("#water-template").html();
+   //Compile the template
+    var waterTemplate = Handlebars.compile(waterTemplateScript);
+    $("#water-information").append(waterTemplate(waterData));
 });
